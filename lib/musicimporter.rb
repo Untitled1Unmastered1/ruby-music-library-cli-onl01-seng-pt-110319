ee534535f7942294dada758669e2d590aa3d0cb3
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-01-13 07:07:13 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -4,11 +4,12 @@ class MusicImporter
   def initialize (path)
     @path = path
   end
+  
   def files
     @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
   end
+  
   def import
     files.each {|file| Song.create_from_filename(file)}
   end
-
 end
